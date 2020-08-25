13bbb1eb7b3b7aded610819c29ea6cb85faa154b
Deon <deondupreez3@gmail.com>
2020-08-26 01:43:46 +0200

    Initial commit and shameless copy

---
+++ WakeOnLan/WakeOnLan/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace WakeOnLan
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string mac = "01-02-03-04-05-06";
+            await WOL.WakeOnLan(mac);
+        }
+    }
+}
