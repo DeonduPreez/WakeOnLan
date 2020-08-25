12f08ea99a1d7afffa804e0650c43efdca7ef664
Deon <deondupreez3@gmail.com>
2020-08-26 01:54:49 +0200

    Made it an actual program

--- WakeOnLan/WakeOnLan/Program.cs
+++ WakeOnLan/WakeOnLan/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace WakeOnLan
 {
@@ -6,8 +7,29 @@ namespace WakeOnLan
     {
         static async Task Main(string[] args)
         {
-            string mac = "01-02-03-04-05-06";
-            await WOL.WakeOnLan(mac);
+            var exit = false;
+
+            do
+            {
+                Console.WriteLine("Please enter the mac address: ");
+                string mac = Console.ReadLine();
+                try
+                {
+                    await WOL.WakeOnLan(mac);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e);
+                }
+
+                Console.WriteLine("Press the escape key to exit, press any other key to try again");
+
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    exit = true;
+                }
+            } while (!exit);
         }
     }
 }
